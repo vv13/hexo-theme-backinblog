45f5380fbc8743ce7d6067d0c4c30d034c69c317
webb <zwhvv13@foxmail.com>
2017-08-14 08:48:45 +0800

    样式微调

---
+++ source/js/vv13.js
@@ -0,0 +1,39 @@
+(function() {
+  window.onload = function() {
+    var goTop = document.getElementById("goTop");
+    //获取页面可视区的高度
+    var clientHeight = document.documentElement.clientHeight;
+    var timer = null;
+    var isTop = true; //回到顶部的过程中如果滚动鼠标滚条会暂停
+
+    //滚动条滚动时触发
+    window.onscroll = function() {
+      //获取滚动条距离顶部的高度，IE:document.documentElement.scrollTop  chrome:document.body.scrollTop
+      var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+      if (osTop >= clientHeight) {
+        goTop.style.display = "block";
+      } else {
+        goTop.style.display = "none";
+      }
+      if (!isTop) {
+        clearInterval(timer);
+      }
+      isTop = false;
+    };
+
+    goTop.onclick = function() {
+      scrollTo(document.body, 0, 100);
+    };
+  };
+
+  function scrollTo(element, to, duration) {
+    if (duration < 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 2;
+
+    setTimeout(function() {
+      element.scrollTop = element.scrollTop + perTick;
+      scrollTo(element, to, duration - 2);
+    }, 10);
+  }
+})();
